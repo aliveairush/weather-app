4fea1434a65c5fb2c086920ee4ef77b10a083410
Aimurzin Ainur <aliveairush@gmail.com>
2023-05-29 00:45:20 +0300

    Added material

--- src/app/weather/features/weather/weather.component.ts
+++ src/app/weather/features/weather/weather.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
 import { Observable } from 'rxjs';
 import { WeatherData } from '../../data-access/types/weather.type';
 import { WeatherService } from '../../data-access/weather.service';
@@ -8,7 +9,7 @@ import { WeatherService } from '../../data-access/weather.service';
 @Component({
   selector: 'app-weather',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, FormsModule],
+  imports: [CommonModule, ReactiveFormsModule, FormsModule, MatCardModule],
   templateUrl: './weather.component.html',
   styleUrls: ['./weather.component.sass'],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { HttpClientModule } from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, {
-  providers: [importProvidersFrom(HttpClientModule)],
+  providers: [
+    importProvidersFrom(HttpClientModule, BrowserAnimationsModule),
+    importProvidersFrom(BrowserAnimationsModule),
+  ],
 });
