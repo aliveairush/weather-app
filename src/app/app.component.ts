34befb102c3216a98eda7bc93d0f2d1cd247b2d2
Aimurzin Ainur <aliveairush@gmail.com>
2023-05-28 17:26:55 +0300

    Move project module to standalone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass'],
+  imports: [],
+  standalone: true,
 })
 export class AppComponent {
   title = 'weather-app';
